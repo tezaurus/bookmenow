2b56b09a860c58f5c4c83887a0bbdb00f126299a
Azarov Sergey <jobguess@mail.ru>
2021-01-31 17:04:32 +0300

    Some updates

--- src/store/modules/booking.ts
+++ src/store/modules/booking.ts
@@ -6,7 +6,7 @@ export default {
   },
 
   actions: {
-    async sendBooking({ commit }, booking) {
+    async sendBooking({ commit }, booking: Booking) {
       // ~ POST запрос на сервер
       // нужно будет переработать метод submitBooking в BookingForm
       // fetch('...', { method: 'POST', body: JSON.stringify(bookingData)})
